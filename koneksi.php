2ade158a80ac48ba3fdcdccae9002d49a016988d
Helen Risky Dwi Wahyuni <riskyhelen1@gmail.com>
2026-04-10 13:10:47 +0700

    menambahkan konfigurasi koneksi database

--- koneksi.php
+++ koneksi.php
@@ -1,7 +1,16 @@
 <?php
-$koneksi = mysqli_connect("127.0.0.1", "root", "", "futsal_booking", 3307);
+$host = "127.0.0.1";
+$user = "root";
+$pass = "";
+$db   = "futsal_booking";
+$port = 3307; // ganti ke 3306 kalau error
+
+$koneksi = mysqli_connect($host, $user, $pass, $db, $port);
 
 if (!$koneksi) {
     die("Koneksi gagal: " . mysqli_connect_error());
 }
+
+// Optional: set charset biar aman
+mysqli_set_charset($koneksi, "utf8mb4");
 ?>

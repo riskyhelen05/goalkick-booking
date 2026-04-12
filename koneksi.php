8c83ccc18e960af62bd7bb46b36c4a90418ade4f
Muhammad Yahya Zahid <myahyazahid11@gmail.com>
2026-04-12 21:05:10 +0700

    fix registration page

--- koneksi.php
+++ koneksi.php
@@ -1,5 +1,5 @@
 <?php
-$koneksi = mysqli_connect("127.0.0.1", "root", "", "futsal_booking", 3307);
+$koneksi = mysqli_connect("127.0.0.1", "root", "", "futsal_booking");
 
 if (!$koneksi) {
     die("Koneksi gagal: " . mysqli_connect_error());
